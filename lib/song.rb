a5558897678b1e4facf2a7fdc7c698b3d9256c0f
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-22 08:42:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,4 +55,17 @@ class Song
   end
 
 
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
 end
